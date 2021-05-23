516ace1f6e958b282817b5d2232838c5b4e94c32
Jake Bown <jakebown@gmail.com>
2021-05-23 23:43:21 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+    	colors: {
+	  		'orange': '#EB6846',
+	  		'darkblue': '#000612'
+	  	},
+	  	backgroundImage: {
+	  		'gradient-sky': 'linear-gradient(180deg, #4657A1 0%, #777FC8 42%)'
+	  	}
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
